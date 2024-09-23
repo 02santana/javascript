9242e7e77563015149a55a913b70265026102b32
02santana <rhuanzinhopvp@gmail.com>
2024-09-23 09:34:41 -0300

    a

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ return num1+num2;
 }
 soma(2, 3);
 console.log(soma(2,3));
+
+console.log()
+
+function areaQuadrado(lado){
+return soma(lado, lado);
+}
+const lado = 5;
+console.log("A área do quadrado é: " + areaQuadrado(lado));
