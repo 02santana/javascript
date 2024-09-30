f79576cb0954aa9e1d152473f30748f876f59107
02santana <rhuanzinhopvp@gmail.com>
2024-09-30 09:19:20 -0300

    a

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-alert('Rhuan e Arthur Xavier')
-function soma(num1,num2){
-return num1+num2;
-}
-soma(2, 3);
-console.log(soma(2,3));
+// alert('Rhuan e Arthur Xavier')
+// function soma(num1,num2){
+// return num1+num2;
+// }
+// soma(2, 3);
+// console.log(soma(2,3));
 
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(5));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(5));
 
-let areaTriangulo=function(base,altura){
-    return (base*altura)/2;
-}
-console.log(areaTriangulo(5,5));
+// let areaTriangulo=function(base,altura){
+//     return (base*altura)/2;
+// }
+// console.log(areaTriangulo(5,5));
 
-let areaTrapezio=function(basemaior,basemenor,altura){
-return (basemaior+basemenor)*altura/2;
-}
-console.log(areaTrapezio(19,10,5));
+// let areaTrapezio=function(basemaior,basemenor,altura){
+// return (basemaior+basemenor)*altura/2;
+// }
+// console.log(areaTrapezio(19,10,5));
 
-let rhuan=prompt('Escolha 3 Números para obter uma média:');
-document.write('1,2,3');
+// let rhuan=prompt('Escolha 3 Números para obter uma média:');
+// document.write('1,2,3');
 
-let num1= parseInt(prompt('1'));
-let num2= parseInt(prompt('2'));
-let num3= parseInt(prompt('3'));
+// let num1= parseInt(prompt('1'));
+// let num2= parseInt(prompt('2'));
+// let num3= parseInt(prompt('3'));
 
-function media(x,y,z){
-    return(x+y+z)/3;
+// function media(x,y,z){
+//     return(x+y+z)/3;
 
-}
-document.write(media(num1,num2,num3));
+// }
+// document.write(media(num1,num2,num3));
 
-let estado= ['Paraná','São Paulo','Minas Gerais','Rio de Janeiro']
+ let estado= ['Paraná','São Paulo','Minas Gerais','Rio de Janeiro']
